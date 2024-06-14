3caf71e5104027d94a2d768254d2fbfec27f5fc4
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-14 17:18:06 +0000

    task 0, float output

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,11 +10,11 @@ import asyncio
 import random
 
 
-async def async_generator() -> Generator[int, None, None]:  # type: ignore
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """
     This is an async coroutine that yields random numbers from 0 - 10.
     """
 
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
