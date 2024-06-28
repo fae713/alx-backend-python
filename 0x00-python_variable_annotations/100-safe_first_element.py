03a6180e5ddbc249c790b21120e532edecc6c55b
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-28 16:44:11 +0000

    advanced task 1

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Duck typing - first element of a sequence
+"""
+from typing import Optional, Any, Iterable
+
+
+def safe_first_element(lst: Iterable[Any]) -> Optional[Any]:
+    """
+    Returns the first element of the iterable if it exists;
+    otherwise, returns None.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
