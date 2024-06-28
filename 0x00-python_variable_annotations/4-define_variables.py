a26e54015dff3280f0ee0ffd40bb6a2cd3eafb58
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-06-28 15:58:11 +0000

    task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Define variables.
+"""
+from typing import List
+
+
+# An integer with a value of 1
+a: int = 1
+
+# A float with a value of 3.14
+pi: float = 3.14
+
+# A boolean with a value of True
+i_understand_annotations: bool = True
+
+# A string with a value of "Holberton"
+school: str = "Holberton"
+
